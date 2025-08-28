b1c9f43475bffd70f9ba8fb977ab47e22b937268
patkeav <keavenyp@gmail.com>
2025-08-27 18:03:59 -0600

    Added Vue, Vite, TypeScript, PrimeVue, and Tailwind for scaffolding

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts"></script>
+
+<template>
+  <Card class="items-center gap-4">
+    <template #title
+      ><h1 class="text-3xl font-bold underline">Build a Color Swatch Grid!</h1></template
+    >
+    <template #content>
+      <label for="saturation">Choose Saturation:</label>
+      <Slider name="saturation" :min="0" :max="100" v-model="saturation" />
+      <label for="lightness">Choose Lightness:</label>
+      <Slider name="lightness" :min="0" :max="100" v-model="lightness" />
+      <p>You saturation: {{ saturation }}</p>
+      <p>You lightness: {{ lightness }}</p>
+    </template>
+  </Card>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+import Slider from 'primevue/slider';
+import Card from 'primevue/card';
+
+const saturation = ref(0);
+const lightness = ref(0);
+</script>
---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+import './index.css';
+
+createApp(App)
+  .use(PrimeVue, {
+    theme: {
+      preset: Aura,
+      options: {
+        prefix: 'p',
+        darkModeSelector: 'system',
+        cssLayer: false
+    }
+    },
+  })
+  .mount('#app');
