9a7d132541610b598ad8973dc37719ddbe36487a
patkeav <keavenyp@gmail.com>
2025-08-28 22:22:35 -0600

    Added loaders, responsiveness, and messaging

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,3 @@
-<script setup lang="ts"></script>
-
 <template>
   <Card class="items-center gap-4">
     <template #title>
@@ -9,22 +7,22 @@
       <div class="flex flex-col gap-4 mb-4">
         <div class="mb-4">
           <label for="saturation" class="block mb-4">Saturation:</label>
-          <InputText v-model.number="saturation" class="mb-4" />
+          <InputNumber suffix="%" v-model.number="saturation" class="mb-4" />
 
           <Slider name="saturation" :min="0" :max="100" v-model="saturation" />
         </div>
         <Divider />
         <div class="mb-4">
           <label for="lightness" class="block mb-4">Lightness:</label>
-          <InputText v-model.number="lightness" class="mb-4" />
+          <InputNumber suffix="%" v-model.number="lightness" class="mb-4" />
 
           <Slider name="lightness" :min="0" :max="100" v-model="lightness" />
         </div>
       </div>
 
-      <Button label="Submit" @click="getSwatches()" v-if="!swatches.length" />
+      <Button label="Submit" @click="getSwatches()" v-if="!swatches.length" :loading="loading" />
       <Button
-        label="Get More Swatches"
+        :label="limit < 360 ? 'Get More Swatches' : 'No more swatches'"
         @click="
           () => {
             start = limit + 1;
@@ -33,20 +31,27 @@
           }
         "
         v-if="swatches.length"
+        :loading="loading"
+        :disabled="limit > 360"
       />
     </template>
   </Card>
-  <DataView :value="swatches" v-if="swatches.length > 0">
-    <template #list="slotProps">
-      <div class="flex flex-col">
-        <div v-for="(item, index) in slotProps.items" :key="index">
-          <div
-            class="flex flex-col sm:flex-row sm:items-center p-6 gap-4"
-            :class="{ 'border-t border-surface-200 dark:border-surface-700': index !== 0 }"
-          >
-            <div class="swatch" :style="{ backgroundColor: item.hex.value }"></div>
-            {{ item.name.value }}
-          </div>
+  <DataView :value="swatches" v-if="swatches.length > 0" layout="grid">
+    <template #header>
+      <div class="flex justify-start">
+        <h2>Color Swatches based on {{ saturation }}% Saturation and {{ lightness }}% Lightness</h2>
+      </div>
+    </template>
+    <template #grid="slotProps">
+      <div class="grid grid-cols-12 gap-4 px-3">
+        <div
+          v-for="(item, index) in slotProps.items"
+          :key="index"
+          class="col-span-12 sm:col-span-3 lg:col-span-2 p-2"
+        >
+          <div class="swatch mx-auto" :style="{ backgroundColor: item.hex.value }"></div>
+          <div class="text-center pt-1">{{ item.name.value }}</div>
+          <div class="text-center pt-1">{{ item.rgb.value }}</div>
         </div>
       </div>
     </template>
@@ -57,12 +62,14 @@ import { ref, watch } from 'vue';
 import Slider from 'primevue/slider';
 import Card from 'primevue/card';
 import Divider from 'primevue/divider';
+import InputNumber from 'primevue/inputnumber';
 import Button from 'primevue/button';
 import API from './util/api.const';
 
 const saturation = ref(0);
 const lightness = ref(0);
 const swatches = ref([]);
+const loading = ref(false);
 let start = 0;
 let limit = 50;
 
@@ -83,26 +90,31 @@ const reset = () => {
 };
 
 const getSwatches = async () => {
-  console.log('getSwatches');
-  const response = await API.getSwatches(saturation.value, lightness.value);
-  const requests = [];
-  console.log(response);
-  for (let i = start; i <= limit; i++) {
-    requests.push(API.getSwatches(i, saturation.value, lightness.value));
+  loading.value = true;
+  try {
+    const response = await API.getSwatches(saturation.value, lightness.value);
+    const requests = [];
+    for (let i = start; i <= limit; i++) {
+      requests.push(API.getSwatches(i, saturation.value, lightness.value));
+    }
+    const responses = await Promise.all(requests);
+    const newSwatches = Array.from(new Set(responses.map((a) => a.name.value)))
+      .map((name) => {
+        return responses.find((a) => a.name.value === name);
+      })
+      .filter((item) => !swatches.value.some((swatch) => swatch.name.value === item.name.value));
+    swatches.value = [...swatches.value, ...newSwatches];
+  } catch (error) {
+    console.error(error);
+  } finally {
+    loading.value = false;
   }
-  const responses = await Promise.all(requests);
-  const newSwatches = Array.from(new Set(responses.map((a) => a.name.value)))
-    .map((name) => {
-      return responses.find((a) => a.name.value === name);
-    })
-    .filter((item) => !swatches.value.some((swatch) => swatch.name.value === item.name.value));
-    debugger;
-  swatches.value = [...swatches.value, ...newSwatches];
 };
 </script>
 <style lang="css" scoped>
 .swatch {
-  width: 100px;
+  width: 100%;
   height: 100px;
+  max-width: 100px;
 }
 </style>
